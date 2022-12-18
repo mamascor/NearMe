4de1d9589422eb306ee4e889d49e497401fd4b7c
Marco Mascorro <marco.mascorroa@gmail.com>
2022-12-18 13:59:25 -0600

    Initial Commit

---
+++ NearMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearMe
+//
+//  Created by Marco Mascorro on 12/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
