1b80a702f000a099070e76ba8285382f57f90b4f
Marco Mascorro <marco.mascorroa@gmail.com>
2022-12-18 14:20:31 -0600

    Started project, added map into view and added user location

--- NearMe/ViewController.swift
+++ NearMe/ViewController.swift
@@ -6,14 +6,74 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    var locationManager: CLLocationManager?
+    
+    lazy var mapview: MKMapView = {
+        let map = MKMapView()
+        map.showsUserLocation = true
+        return map
+    }()
+    
+    lazy var searchTextView: UITextField = {
+        let tf = UITextField()
+        tf.layer.cornerRadius = 10
+        tf.clipsToBounds = true
+        tf.backgroundColor = UIColor.white
+        tf.placeholder = "Search"
+        tf.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        tf.leftViewMode = .always
+        
+        return tf
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Init locationmanager
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.requestLocation()
+        
+        
+        configureUserInterface()
     }
 
+    
+    private func configureUserInterface(){
+        view.addSubview(mapview)
+        mapview.addConstraintsToFillView(view)
+        
+        mapview.addSubview(searchTextView)
+        
+        searchTextView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 20, paddingLeft: 12, paddingRight: 12, height: 50)
+    }
+    
+    func requestPermissions(){
+        
+        
+        
+        
+    }
 
 }
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+    }
+    
+    
+}
+
 
